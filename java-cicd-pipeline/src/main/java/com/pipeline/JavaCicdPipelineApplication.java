4d917f0472fbbe740fd62881acf28a07e7f5531f
SureshDP9 <suresh.doddi@techolution.com>
2023-02-07 11:47:27 +0530

    added microservice

---
+++ java-cicd-pipeline/src/main/java/com/pipeline/JavaCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCicdPipelineApplication.class, args);
+	}
+
+}
